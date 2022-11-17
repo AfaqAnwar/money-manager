f9949b42482911e2cc319f7fcae40758739dd21d
Afaq Anwar <anwarafaq@protonmail.com>
2022-11-16 21:49:12 -0500

    Remove unused imports

--- lib/auth/main_page.dart
+++ lib/auth/main_page.dart
@@ -1,7 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:moneymanager/auth/auth_page.dart';
-import 'package:moneymanager/pages/login_page.dart';
 
 import '../pages/home_page.dart';
 
