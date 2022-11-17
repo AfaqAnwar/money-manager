9e888ac2520ed2a50b265897f85ba3eda00ab807
Afaq Anwar <anwarafaq@protonmail.com>
2022-11-16 22:40:51 -0500

    Remove unused imports

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
+// ignore_for_file: depend_on_referenced_packages
+
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:moneymanager/auth/main_page.dart';
-import 'pages/login_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
