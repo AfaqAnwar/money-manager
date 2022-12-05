04e0672d8d27aa100c5af470e5e3d9507807de6a
Mikey Delgado <37640551+mikeydelgado@users.noreply.github.com>
2022-12-04 19:16:57 -0500

    "more visualization and calculator stuff"

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
-// ignore_for_file: depend_on_referenced_packages
-
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:moneymanager/auth/main_page.dart';
 import 'package:moneymanager/pages/login_page.dart';
+import 'package:moneymanager/pages/calculator_page.dart';
+import 'package:moneymanager/pages/register_page.dart';
 import 'package:moneymanager/pages/survey.dart';
 import 'package:moneymanager/pages/visualize_page.dart';
 import 'package:fl_chart/fl_chart.dart';
@@ -25,7 +25,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: VisualizePage(),
+      home: CalculatorPage(),
     );
   }
 }
