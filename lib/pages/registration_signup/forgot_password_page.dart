926ec24164d2c0eac08d6b8c050b3c88d409eabb
Afaq Anwar <anwarafaq@protonmail.com>
2022-12-05 14:16:08 -0500

    UI Changes

--- lib/pages/registration_signup/forgot_password_page.dart
+++ lib/pages/registration_signup/forgot_password_page.dart
@@ -1,11 +1,14 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:moneymanager/utils/colors.dart';
 
 // Reset Password Page
 class ForgotPasswordPage extends StatefulWidget {
   const ForgotPasswordPage({Key? key}) : super(key: key);
 
   @override
+  // ignore: library_private_types_in_public_api
   _ForgotPasswordPageState createState() => _ForgotPasswordPageState();
 }
 
@@ -26,7 +29,20 @@ class _ForgotPasswordPageState extends State<ForgotPasswordPage> {
           context: context,
           builder: (context) {
             return AlertDialog(
-              content: Text('Password Reset Link Sent! Check your inbox.'),
+              shape: const RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(20.0))),
+              titleTextStyle: GoogleFonts.roboto(
+                  color: AppColor.customLightGreen,
+                  fontWeight: FontWeight.w800,
+                  fontSize: 24),
+              contentTextStyle:
+                  GoogleFonts.roboto(color: Colors.black, fontSize: 16),
+              content: Text(
+                'Password Reset Link Sent! Check your inbox.',
+                style: GoogleFonts.roboto(
+                    color: AppColor.customDarkGreen,
+                    fontWeight: FontWeight.w600),
+              ),
             );
           });
     } on FirebaseAuthException catch (e) {
@@ -44,21 +60,21 @@ class _ForgotPasswordPageState extends State<ForgotPasswordPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.blue,
+        backgroundColor: AppColor.customDarkGreen,
         elevation: 0,
       ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 25),
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 25),
             child: Text(
               'Provide the email associated with your account in order to get a reset link.',
               textAlign: TextAlign.center,
               style: TextStyle(fontSize: 18),
             ),
           ),
-          SizedBox(height: 10),
+          const SizedBox(height: 10),
           Padding(
             padding: const EdgeInsets.symmetric(horizontal: 25),
             child: Container(
@@ -71,7 +87,7 @@ class _ForgotPasswordPageState extends State<ForgotPasswordPage> {
                 padding: const EdgeInsets.only(left: 20.0),
                 child: TextField(
                   controller: emailController,
-                  decoration: InputDecoration(
+                  decoration: const InputDecoration(
                     border: InputBorder.none,
                     hintText: 'Email',
                   ),
@@ -79,11 +95,17 @@ class _ForgotPasswordPageState extends State<ForgotPasswordPage> {
               ),
             ),
           ),
-          SizedBox(height: 10),
+          const SizedBox(height: 10),
           MaterialButton(
             onPressed: passwordReset,
-            child: Text('Reset Password'),
-            color: Colors.lightBlue,
+            color: AppColor.customDarkGreen,
+            child: Text('Reset Password',
+                textAlign: TextAlign.center,
+                style: GoogleFonts.roboto(
+                  fontSize: 17,
+                  color: Colors.white,
+                  fontWeight: FontWeight.w500,
+                )),
           ),
         ],
       ),
